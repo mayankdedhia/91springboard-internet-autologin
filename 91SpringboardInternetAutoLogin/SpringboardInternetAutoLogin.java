2da3693fabde2d8ef27bbdc641eec18ea5c199e5
Mayank Dedhia <mayankdedhia@gmail.com>
2019-12-30 13:33:59 +0530

    Change the way how the task is run at regular intervals

--- 91SpringboardInternetAutoLogin/SpringboardInternetAutoLogin.java
+++ 91SpringboardInternetAutoLogin/SpringboardInternetAutoLogin.java
@@ -2,56 +2,41 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
 
 public class SpringboardInternetAutoLogin
 {
 	public static void main(String args[])
 	{
-		TimerTask task = new TimerTask() 
-		{
-			@Override
-			public void run() 
-			{
-				boolean internetAvailable = false;
-				internetAvailable = isInternetAvailable();
-				if(!internetAvailable)
+		ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+		scheduler.scheduleWithFixedDelay(new Runnable() {
+			public void run() {
+				try 
 				{
-					try 
-					{
+					boolean internetAvailable = false;
+					internetAvailable = isInternetAvailable();
+					if(!internetAvailable)
+					{					
+						System.out.println("Internet is on working or needs to be connected to");
 						portalAutoLogin();
-					} catch (Exception e) {
-						e.printStackTrace();
-						System.out.println("Failed to connect to internet");
 					}
-				}
-				else
-				{
-					System.out.println("Internet is Working");
+					else
+					{
+						System.out.println("Internet is Working");
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.out.println("Failed to run the complete task");
 				}
 			}
-		};
-
-		Timer timer = new Timer();
-		long delay = 0;
-		long intevalPeriod = 15 * 1000; 
-
-		// schedules the task to be run in an interval 
-		timer.scheduleAtFixedRate(task, delay,
-				intevalPeriod);
-
+		}, 15, 15, TimeUnit.SECONDS);
 	}
 
 	public static boolean isInternetAvailable()
@@ -81,39 +66,44 @@ public class SpringboardInternetAutoLogin
 			in.close();
 
 		} catch (Exception e) {
+			System.out.println("Unable check internet status");
 			e.printStackTrace();
 		}
 
 		if (html == null || html.indexOf("91Springboard Captive Portal - Login") > -1 || html.indexOf("Error 302: Hotspot login required") > -1 )
 		{
-			//System.out.println("internet not on");
 			return false;
 		}
 		else
 		{
-			//System.out.println("internet on");
 			return true;
 		}
 	}
 
 	public static void portalAutoLogin() throws InterruptedException
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Utils\\chromedriver.exe");
-		
-		ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        
-		WebDriver driver = new ChromeDriver(chromeOptions);
-		driver.get("http://portal.91springboard.com/login");
-		driver.manage().window().maximize();
-		driver.switchTo().frame("captive-portal");
-		driver.findElement(By.id("emailField")).sendKeys("dummy@test.com");
-		driver.findElement(By.id("passwordField")).sendKeys("dummy.");
-		
-		driver.findElements(By.cssSelector(".form-submit .submit-button")).get(0).click();
-		Thread.sleep(5000);
-		driver.close();
-		System.out.println("Yehhhhhhhhh.....!!");
-		System.out.println("Ho gaya LogIn");
+		try {
+			System.setProperty("webdriver.chrome.driver", "C:\\Utils\\chromedriver.exe");
+			
+			ChromeOptions chromeOptions = new ChromeOptions();
+	        chromeOptions.addArguments("--headless");
+	        
+			WebDriver driver = new ChromeDriver(chromeOptions);
+			driver.get("http://portal.91springboard.com/login");
+			driver.manage().window().maximize();
+			driver.switchTo().frame("captive-portal");
+			driver.findElement(By.id("emailField")).sendKeys("dummy@test.com");
+			driver.findElement(By.id("passwordField")).sendKeys("dummy.");
+			
+			driver.findElements(By.cssSelector(".form-submit .submit-button")).get(0).click();
+			Thread.sleep(5000);
+			driver.close();
+			System.out.println("Yehhhhhhhhh.....!!");
+			System.out.println("Ho gaya LogIn");
+		}
+		catch (Exception e) {
+			System.out.println("Unable to login via browser");
+			e.printStackTrace();
+		}
 	}
 }
